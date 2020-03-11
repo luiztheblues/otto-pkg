12c7f4dbe74efa0ecc91162e09e23a5d4a544f62
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2020-03-11 14:50:02 -0300

    Nova versãodo otto-torrent.py.

--- otto-torrent.py
+++ otto-torrent.py
@@ -9,66 +9,73 @@ OTTO_PKG está sobre a Licença BSD Clause-2 (Simplificada).
 
 Dependências:
 
- >python 3.4=< 
- >libtorrent-rasterbar
+>python 3.4=< 
+>libtorrent-rasterbar
 
-Sintaxe
-python3.? otto-torrent.py -f <file>
+Notas para próximas versões:
+>ajustar manualmente a prioridade de banda do download;
+>começar a se usar link magnético como uma opção para os arquivos .torrent;
+>modo para criação de .torrents (acionado via uma flag);
+>adicionar suporte à criptografia (se possível);
+>criar patch para tradução em inglês e em outros idiomas;
+
+Sintaxe:
+python3.? otto-torrent.py -s -f <file> -o <directory | file-basename>
 '''
 
 
 import libtorrent as lt
-
-from sys import argv
-
+import argparse, os
 
 
-seed = True
+argumento = argparse.ArgumentParser()
+argumento.add_argument("--file", "-f", required=True,
+                       help="Arquivo torrent para fazer download")
+argumento.add_argument("--seeding", "-S", action="store_true",
+                       help="Continuar semeando após o término do download")
+argumento.add_argument("--infos", "-i",
+                       help="Mostrar informações do torrent")
+argumento.add_argument("--output", "-O", action="store_true",
+                       help="Saída final do arquivo; nome")
+#argumento.add_argument("--download-priority", "-P", default=lt.pri,
+#                       help="Definir prioridade ao download")
+#argumento.add_argument("--link", "-L",
+#                       help="Criar um link magnético através de um arquivo")
+argumento.add_argument("--dir", "-d",
+					   help="Especificar diretório onde o donwload ficará salvo")
+argumentos = argumento.parse_args()
 
-sessao = lt.session({'listen_interfaces': '0.0.0.0:10880'})
-
-info = lt.torrent_info(argv[2]) # Irá mostrar infos sobre o torrent na memória
-
-# ex: 0x55bed417ab00>. O "argv[2]" é o diretório do torrent
-
-
-
-try:
-
-    if argv[1] == '-f':
-
-        local = sessao.add_torrent({'ti': info, 'save_path': '/tmp/otto_pkg/'})
-
-        torrent = local.status() # Irá mostrar o nome do arquivo (não será nome do arquivo
-
-        # que está no computador, e sim, o que está no torrent final após ser baixado)
-
+sessao = lt.session({'listen_interfaces': '0.0.0.0:10881'})
 
 
-except Exception as error:
+def iniciar_download(arquivo):
+    try:
+        info = lt.torrent_info(arquivo)
+        if argumentos.dir:
+            if os.path.isdir(argumentos.dir):
+                local = sessao.add_torrent({'ti': info, 'save_path': argumentos.dir})
+            else:
+                print(">>> Diretório inválido")
+        else:
+            local = sessao.add_torrent({'ti': info, 'save_path': '/tmp/otto_pkg/'})
+        torrent = local.status()
+        if argumentos.file and argumentos.seeding:
+            while True:
+                torrent = local.status()
+                print("\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s" % (
+                 torrent.progress * 100, torrent.download_rate / 1000, torrent.upload_rate / 1000,
+                 torrent.num_peers, torrent.state), end=" ")
+            print("\n>>> Download concluído.\n>>>Semeando")
+        elif argumentos.file:
+            while not torrent.is_seeding:
+                torrent = local.status()
+                print("\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s" % (
+        		 torrent.progress * 100, torrent.download_rate / 1000, torrent.upload_rate / 1000,
+        		 torrent.num_peers, torrent.state), end=" ")
+        print("\n>>> Download concluído!")
+    except KeyboardInterrupt:
+        print("\n>>> Saindo do programa...\n")
+    except Exception as error:
+        print(">>> Houve algum erro: {}".format(error))
 
-    print('Algum erro ocorreu: {}'.format(error))
-
-
-
-print('Baixando o pacote: {}'.format(torrent.name))
-
-
-
-while True:
-
-    torrent = local.status()
-
-    print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
-
-        torrent.progress * 100, torrent.download_rate / 1000, torrent.upload_rate / 1000,
-
-        torrent.num_peers, torrent.state), end=' ')
-
-    if torrent.is_seeding and seed == False:
-
-        break
-
-print('Download de {} concluído!'.format(torrent.name))
-
-
+iniciar_download(argumentos.file)
